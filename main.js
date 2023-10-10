23afd7fb0f332bed2bd9d373eced69b18404fb33
Nikita <4helovekov@gmail.com>
2023-10-10 19:22:38 +0500

    Download and configure MSW

--- main.js
+++ main.js
@@ -22,3 +22,20 @@ document.querySelector("#app").innerHTML = `
 `
 
 setupCounter(document.querySelector("#counter"))
+
+const runApp = async () => {
+  switch (process.env.NODE_ENV) {
+    case "development":
+      await import("./src/mocks/browser.js")
+        .then(async ({ worker }) => {
+          await worker.start().then(() => {
+            console.debug("App dev run")
+          })
+        })
+  }
+}
+
+runApp()
+  .catch((err) => {
+    console.error(err)
+  })
