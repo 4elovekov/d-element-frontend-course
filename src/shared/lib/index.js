e5341d9286c392ac891fe7fa9c0fcd5fe19bccc6
Nikita <4helovekov@gmail.com>
2023-10-12 22:12:01 +0500

    delete favicon link

--- src/shared/lib/index.js
+++ src/shared/lib/index.js
@@ -4,7 +4,6 @@ export const getPage = ({ body = ``, title = ``, meta = [] }) => {
     <html lang="ru">
       <head>
         <meta charset="UTF-8" />
-        <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         ${meta.map(item => item())}
         <title>${title}</title>
