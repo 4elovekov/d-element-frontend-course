eb2da5cb768c3debcd6fd538e0b23faf30a56e6e
Nikita <4helovekov@gmail.com>
2023-10-24 13:19:40 +0500

    add handlers

--- src/shared/api/handlers.js
+++ src/shared/api/handlers.js
@@ -18,4 +18,37 @@ export const handlers = [
       }),
     )
   }),
+
+  rest.get("/api/getProducts", (req, res, ctx) => {
+    return res(
+      ctx.status(200), ctx.json({
+        "isSuccess": "true",
+        "data": {
+            "idProduct": "123",
+            "label": "Бесплатный офлайн курс",
+            "productName": "«Frontend-разработчик»",
+            "category": [
+                "Веб-разработка",
+                "Мобильная разработка"
+            ],
+            "imageSrc": "/images/course-image.png",
+            "registration": {
+                    "startDate": "298347302984",
+                    "endDate": "239847320984"
+            },
+            "startCourse": "2389047320",
+            "price": 10000,
+            "isPopular": true
+        }
+      })
+    )
+  }),
+
+  rest.post("/api/cartSubmit", (req, res, ctx) => {
+    return res(
+      ctx.status(200), ctx.json({
+        "isSuccess": true,
+      })
+    )
+  }),
 ]
