82a33d12586b7f7a438c780e3b43ce4c34bb2c5b
Nikita pc <4helovekov@gmail.com>
2023-10-27 18:52:18 +0500

    add builder

--- src/widgets/filter/model/index.js
+++ src/widgets/filter/model/index.js
@@ -1,3 +1,5 @@
+import Builder from "../../../shared/lib/builder"
+
 export default class FilterModel {
 
     static selectors = {
@@ -16,10 +18,11 @@ export default class FilterModel {
         FilterModel.instance = document.querySelector(FilterModel.selectors.instanceSelector);
         this.inputs = Array.from(document.querySelectorAll(`[${FilterModel.selectors.checkboxSelectors}]`))
         // this.url = new URL(window.location.href)
-        this.url = new URL(window.location.href);
-        this.params = new URLSearchParams(this.url.search);
-        console.debug(this.url)
-        console.debug(this.params.toString())
+
+        // this.url = new URL(window.location.href);
+        // this.params = new URLSearchParams(this.url.search);
+
+        this.url = new Builder(window.location.href);
         this.init()
     }
 
@@ -31,17 +34,20 @@ export default class FilterModel {
         this.inputs.forEach(input => {
             if (input.checked) {
                 input.setAttribute("checked", "")
-                this.params.set(`${input.value}`, true);
+                this.url.addParam(input.value, true);
             } else {
                 input.removeAttribute("checked")
-                this.params.delete(`${input.value}`)
+                this.url.deleteParam(input.value)
             }
             // input.checked ? input.setAttribute("checked", "") : input.removeAttribute("checked")
-            console.debug(this.params.toString())
-            console.debug(input)
         })
-        console.debug(this.url)
-        fetch (this.url)
+
+        // this.new_url = new URL(`${this.url.origin}${this.url.pathname}?${this.params}`);
+        // console.debug("new url: ", this.new_url)
+
+        console.debug(this.url.stringParams())
+        console.debug(this.url.stringUrl())
+        fetch (this.url.stringUrl())
             .then(response => console.debug(response.json()))
     }
 
