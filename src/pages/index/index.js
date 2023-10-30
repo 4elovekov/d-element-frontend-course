73151cfa5546124a58dc841a534c5af8c1ea6664
Nikita <4helovekov@gmail.com>
2023-10-30 16:07:34 +0500

    update filter styles

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,6 +1,4 @@
 import { getPage } from "../../shared/lib/index.js";
-import { Button } from "../../shared/ui/button/index.js";
-import { Input } from "../../shared/ui/input/index.js";
 import { metaTag } from "../../shared/lib/meta.js";
 import { Header } from "../../widgets/header/index.js";
 import { Footer } from "../../widgets/footer/index.js";
@@ -13,45 +11,10 @@ export default () => {
             ${Header()}
             <main>
                 <div class="container">
-                    <h1>Главная</h1>
+                    <h1>Популярные товары</h1>
                 </div>
 
-                <div class="form">
-                    <h2 class="name">Введите Ваше имя</h2>
-                    ${Input({
-                        extraAttrs: {
-                            "type": "text",
-                            "id": "input",
-                            "required minlength": "4",
-                            "maxlength": "15"
-                        }
-                    })}
-                    
-                    ${Button({
-                        label: "Click",
-                        extraAttrs: {
-                            "id": "btn"
-                        },
-                        extraClasses: {
-                            hidden: false,
-                            disabled: false,
-                        }
-                    })}
-                </div>
-
-                <nav>
-                    <ul>
-                        <li>
-                            <a href="/">Главная</a>
-                        </li>
-                        <li>
-                            <a href="/cart.html">Корзина</a>
-                        </li>
-                        <li>
-                            <a href="/catalog.html">Каталог</a>
-                        </li>
-                    </ul>
-                </nav>
+                
             </main>
             ${Footer()}
         `,
